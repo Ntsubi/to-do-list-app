9676d54866d9be61f88cd4917b00a0143a92df17
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-10-21 12:20:50 +0200

    Added function to sort items by dragging

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,31 @@
-function newItem(){
-//Adding a new item to the list of items
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-li.append(inputValue);
+function newItem() {
+    //Adding a new item to the list of items
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
 
-if (inputValue === '') {
-    alert('You must write something!')
-}else{
-    $('#list').append(li);
-}
+    if (inputValue === '') {
+        alert('You must write something!')
+    } else {
+        $('#list').append(li);
+    }
 
-//Crossing out an item from the list of items
-li.on('dblclick', crossOut);
-function crossOut() {
-    li.toggleClass('strike');
-}
+    //Crossing out an item from the list of items
+    li.on('dblclick', crossOut);
+    function crossOut() {
+        li.toggleClass('strike');
+    }
 
-//Adding the delete button "x"
-let crossOutButton = $('<crossOutButton></crossOutButton>')
+    //Adding the delete button "x"
+    let crossOutButton = $('<crossOutButton></crossOutButton>')
     crossOutButton.append(document.createTextNode('X'));
     li.append(crossOutButton);
 
-//Adding class "delete" (display:none) from css
-crossOutButton.on('click', deleteListItem);
-function deleteListItem (){
-    li.addClass('delete');
-}
+    //Adding class "delete" (display:none) from css
+    crossOutButton.on('click', deleteListItem);
+    function deleteListItem() {
+        li.addClass('delete');
+    }
+    //Reordering items
+    $('#list').sortable();
 }
