fc10490e3cf27f9bbea6d20dce61ae881208785f
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-10-21 12:08:30 +0200

    Edited syntax for adding items

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ function newItem(){
 //Adding a new item to the list of items
 let li = $('<li></li>');
 let inputValue = $('#input').val();
+li.append(inputValue);
 
 if (inputValue === '') {
     alert('You must write something!')
